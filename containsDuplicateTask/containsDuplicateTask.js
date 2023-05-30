77c3789769fc4bc3721a7873af8e0161ad5d6584
All1in <bm3023068@gmail.com>
2023-05-30 11:11:04 +0300

    duplicates on leetcode

---
+++ containsDuplicateTask/containsDuplicateTask.js
@@ -0,0 +1,14 @@
+let containsDuplicate = function(nums) {
+   const numSet = new Set()
+    for (let i = 0; i < nums.length; i++) {
+        if(numSet.has(nums[i])) {
+            return true
+        }
+        numSet.add(nums[i])
+    }
+    return false
+}
+
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
