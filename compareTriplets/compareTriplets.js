3cd1cecb02bf515d6ef8c551d6cd40105e0a8cb4
All1in <bm3023068@gmail.com>
2023-07-28 17:56:48 +0300

    compare triplet js

--- compareTriplets/compareTriplets.js
+++ compareTriplets/compareTriplets.js
@@ -1,9 +1,15 @@
 function compareTriplets(a, b) {
-    const resultArray = []
+    let aliceScore = 0;
+    let bobScore = 0;
 
-    for (let i = 0; i < a.length; i++) {
-
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            aliceScore++;
+        } else if (a[i] < b[i]) {
+            bobScore++;
+        }
     }
-}
 
+    return [aliceScore, bobScore];
+}
 console.log(compareTriplets([5, 6, 7], [3, 6, 10]))
